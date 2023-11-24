d93625671bd0275271487aa09ce2529ea10634d2
MaximilianoBonnet <maxbonn@hotmail.com>
2023-11-24 16:51:58 -0300

    subo archivos

---
+++ script.js
@@ -0,0 +1,22 @@
+// Mostrar el modal al cargar la página
+window.onload = function () {
+    document.getElementById('login-modal').style.display = 'block';
+};
+
+// Función para cerrar el modal
+function closeModal() {
+    document.getElementById('login-modal').style.display = 'none';
+}
+
+// Función para verificar la contraseña y acceder a la documentación
+function login() {
+    var password = document.getElementById('password').value;
+
+    // Verifica la contraseña (puedes cambiarla según sea necesario)
+    if (password === '123456') {
+        closeModal(); // Cierra el modal si la contraseña es correcta
+        window.location.href = 'index.html'; 
+    } else {
+        alert('Contraseña incorrecta. Intenta de nuevo.'); // Muestra un mensaje de error
+    }
+}
